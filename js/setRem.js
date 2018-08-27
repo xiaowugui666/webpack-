aaf9cb696d30834a5092918c061246e312a10290
wuming <wuming@51zan.com>
2018-08-27 16:26:04 +0800

    中转页面

--- js/setRem.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  var rem=0;
-  var htmlDOM=document.documentElement;
-  var currentWidth=htmlDOM.clientWidth;
-  rem=currentWidth/3.75;
-  htmlDOM.style.fontSize=rem+'px';
-})()
---
+++ src/js/transfer.js
@@ -0,0 +1,6 @@
+import config from '../config'
+
+window.onload=function(){
+    // console.log(location)
+    // window.location.href= 'https://retail.51zan.com/public/mps/auth?app_id=wx2840761b6d4a722a&redirect_uri=https://retail-static-develop.51zan.com/red-packet/index.html?id=7'
+}
