bb87c5ac54b09b7585b8cb841d6687988e76bdc1
wuming <wuming@51zan.com>
2018-08-30 20:13:51 +0800

    111

--- src/js/transfer.js
+++ src/js/transfer.js
@@ -2,7 +2,7 @@ $(function(){
     var app_id = getParam('app_id')
     var id = getParam('id')   
     var host = location.host
-    window.location.href= 'https://retail.51zan.com/public/mps/auth?scope=snsapi_userinfo&app_id='+app_id+'&redirect_uri='+encodeURIComponent("https://"+host+"/red-packet/index.html?id="+id)
+    window.location.href= 'https://retail.51zan.com/public/mps/auth?scope=snsapi_userinfo&app_id='+app_id+'&redirect_uri='+encodeURIComponent("https://"+host+"/red-packet/transfer.html?id="+id)
     function getParam(name) {
         var paramUrl = window.location.search.substr(1);
         var paramStrs = paramUrl.split('&');
