9a16e01fd6968eba720c922f82c7ac0ce97c7d9f
罗龙 <luolong@51zan.com>
2018-11-01 18:02:30 +0800

    增加css编译器

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,19 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
                 test: /\.js$/,
                 exclude:/(node_modules|bower_components)/,
                 use: {
